32f747a07c0a730c48d8751ea23e02c464a35feb
Michael Mak <MichaelMak@MAK.local>
2016-02-23 20:48:34 +0000

    gg

--- myWeb/testing.js
+++ myWeb/testing.js
@@ -1,12 +1,21 @@
 // Define the osMap variable
-var osMap;
+var osMap; 
+var mapPointX;
+var mapPointY;
 
 // This function creates the map and is called by the div in the HTML
 function init()
 {
     // Create new map
     osMap = new OpenSpace.Map('map');
+    mapPointX = 400000;
+    mapPointY = 400000;
     // Set map centre in National Grid Eastings and Northings and select zoom level 8
-	osMap.setCenter(new OpenSpace.MapPoint(400000, 400000), 8);
-            
+	osMap.setCenter(new OpenSpace.MapPoint(mapPointX, mapPointY), 8);            
+}
+
+function left()
+{
+	mapPointX = mapPointX-100;
+	osMap.setCenter(new OpenSpace.MapPoint(mapPointX, mapPointY), 8);
 }
