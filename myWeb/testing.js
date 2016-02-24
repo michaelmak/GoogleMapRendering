221d5da92d0724fe5f857400e367a510e2d84c35
Michael Mak <MichaelMak@MAK.local>
2016-02-24 18:16:33 +0000

    gg

--- myWeb/testing.js
+++ myWeb/testing.js
@@ -14,8 +14,110 @@ function init()
 	osMap.setCenter(new OpenSpace.MapPoint(mapPointX, mapPointY), 8);            
 }
 
+// Assign a new markers layer to a variable
+    
+    var markers = new OpenLayers.Layer.Markers("Markers");
+
+// Add the layer to the map    
+    
+
 function left()
 {
-	mapPointX = mapPointX-100;
+	mapPointX = mapPointX-1000;
 	osMap.setCenter(new OpenSpace.MapPoint(mapPointX, mapPointY), 8);
 }
+
+function right()
+{
+	mapPointX = mapPointX+1000;
+	osMap.setCenter(new OpenSpace.MapPoint(mapPointX, mapPointY), 8);
+}
+
+function up()
+{
+	mapPointY = mapPointY+1000;
+	osMap.setCenter(new OpenSpace.MapPoint(mapPointX, mapPointY), 8);
+}
+
+function down()
+{
+	mapPointY = mapPointY-1000;
+	osMap.setCenter(new OpenSpace.MapPoint(mapPointX, mapPointY), 8);
+}
+
+function centerValue()
+{
+	//var myCenter=document.getElementById("myCenter");
+	//document.write(osMap.getCenter());
+
+	document.getElementById("myCenter").innerHTML = "Focus Center: X " + mapPointX + " Y " + mapPointY;
+	var pos = new OpenSpace.MapPoint(mapPointX, mapPointY);
+	var marker = new OpenLayers.Marker(pos);
+    markers.addMarker(marker);
+}
+
+function ULeft()
+{
+	//var myCenter=document.getElementById("myCenter");
+	//document.write(osMap.getCenter());
+
+	document.getElementById("myULeft").innerHTML = "Upper Left: X " + (mapPointX-1000) + " Y " + (mapPointY+2000);
+	var pos = new OpenSpace.MapPoint(mapPointX-1000, mapPointY+2000);
+	var marker = new OpenLayers.Marker(pos);
+    markers.addMarker(marker);
+}
+
+function URight()
+{
+	//var myCenter=document.getElementById("myCenter");
+	//document.write(osMap.getCenter());
+
+	document.getElementById("myURight").innerHTML = "Upper Right: X " + (mapPointX+2000) + " Y " + (mapPointY+2000);
+	var pos = new OpenSpace.MapPoint(mapPointX+2000, mapPointY+2000);
+	var marker = new OpenLayers.Marker(pos);
+    markers.addMarker(marker);
+
+}
+
+function BLeft()
+{
+	//var myCenter=document.getElementById("myCenter");
+	//document.write(osMap.getCenter());
+
+	document.getElementById("myBLeft").innerHTML = "Bottom Left: X " + (mapPointX-1000) + " Y " + (mapPointY-1000);
+	var pos = new OpenSpace.MapPoint(mapPointX-1000, mapPointY-1000);
+	var marker = new OpenLayers.Marker(pos);
+    markers.addMarker(marker);
+
+}
+
+function BRight()
+{
+	//var myCenter=document.getElementById("myCenter");
+	//document.write(osMap.getCenter());
+
+	document.getElementById("myBRight").innerHTML = "Bottom Right: X " + (mapPointX+2000) + " Y " + (mapPointY-1000);
+	var pos = new OpenSpace.MapPoint(mapPointX+2000, mapPointY-1000);
+	var marker = new OpenLayers.Marker(pos);
+    markers.addMarker(marker);
+
+}
+
+function switchMarker(){
+    try{
+		var button = document.getElementById("switchMarker");
+		if(button.value == "Switch Marker On"){
+			osMap.addLayer(markers);
+	    	button.value = "Switch Marker Off";
+		}
+		else{
+			osMap.removeLayer(markers);
+			button.value = "Switch Marker On";
+		}    
+	}
+    catch(e){
+		error(e, "switchMarker");
+    }
+}
+
+
