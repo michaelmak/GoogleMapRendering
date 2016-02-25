f03114f33cbd73900be936a277df453b90185d38
Michael Mak <MichaelMak@MAK.local>
2016-02-25 20:46:12 +0000

    updated point2lonlat & simple GMap

--- myWeb/testing.js
+++ myWeb/testing.js
@@ -1,7 +1,12 @@
 // Define the osMap variable
 var osMap; 
+var GMAP;
 var mapPointX;
 var mapPointY;
+var focusPoint;
+var lon;
+var lat;
+
 
 // This function creates the map and is called by the div in the HTML
 function init()
@@ -10,47 +15,48 @@ function init()
     osMap = new OpenSpace.Map('map');
     mapPointX = 400000;
     mapPointY = 400000;
+    focusPoint = new OpenSpace.MapPoint(mapPointX, mapPointY);
+    osMap.gridProjection = new OpenSpace.GridProjection();
     // Set map centre in National Grid Eastings and Northings and select zoom level 8
-	osMap.setCenter(new OpenSpace.MapPoint(mapPointX, mapPointY), 8);            
+	osMap.setCenter(focusPoint, 8);            
 }
 
-// Assign a new markers layer to a variable
-    
-    var markers = new OpenLayers.Layer.Markers("Markers");
-
-// Add the layer to the map    
-    
+// Assign a new markers layer to a variable 
+var markers = new OpenLayers.Layer.Markers("Markers");
 
 function left()
 {
 	mapPointX = mapPointX-1000;
-	osMap.setCenter(new OpenSpace.MapPoint(mapPointX, mapPointY), 8);
+	focusPoint = new OpenSpace.MapPoint(mapPointX, mapPointY);
+	osMap.setCenter(focusPoint, 8);
 }
 
 function right()
 {
 	mapPointX = mapPointX+1000;
-	osMap.setCenter(new OpenSpace.MapPoint(mapPointX, mapPointY), 8);
+	focusPoint = new OpenSpace.MapPoint(mapPointX, mapPointY);
+	osMap.setCenter(focusPoint, 8);
 }
 
 function up()
 {
 	mapPointY = mapPointY+1000;
-	osMap.setCenter(new OpenSpace.MapPoint(mapPointX, mapPointY), 8);
+	focusPoint = new OpenSpace.MapPoint(mapPointX, mapPointY);
+	osMap.setCenter(focusPoint, 8);
 }
 
 function down()
 {
 	mapPointY = mapPointY-1000;
-	osMap.setCenter(new OpenSpace.MapPoint(mapPointX, mapPointY), 8);
+	focusPoint = new OpenSpace.MapPoint(mapPointX, mapPointY);
+	osMap.setCenter(focusPoint, 8);
 }
 
 function centerValue()
 {
 	//var myCenter=document.getElementById("myCenter");
 	//document.write(osMap.getCenter());
-
-	document.getElementById("myCenter").innerHTML = "Focus Center: X " + mapPointX + " Y " + mapPointY;
+    document.getElementById("myCenter").innerHTML = "Focus Center: X " + mapPointX + " Y " + mapPointY;
 	var pos = new OpenSpace.MapPoint(mapPointX, mapPointY);
 	var marker = new OpenLayers.Marker(pos);
     markers.addMarker(marker);
@@ -118,6 +124,13 @@ function switchMarker(){
     catch(e){
 		error(e, "switchMarker");
     }
+}
+
+function point2lonlat(){
+	var lonlat = osMap.gridProjection.getLonLatFromMapPoint(focusPoint);
+	lon = lonlat.lon;
+	lat = lonlat.lat;
+	document.getElementById("myLonlatCenter").innerHTML = "LonLat Center: lon " + lon + " lat " + lat;
 }
 
 
