12749607eadb91c67683e36528d39576439b946b
Michael Mak <MichaelMak@MAK.local>
2016-03-07 22:21:46 +0000

    loadCoordinate attempted, cant pass E/N to input

--- myWeb/testing.js
+++ myWeb/testing.js
@@ -7,6 +7,8 @@ var focusPoint;
 var lon;
 var lat;
 var ULeftCoodinate;
+var easting4Trans;
+var northing4Trans; 
 
 function error(e, s){
     alert(e+" in "+s);
@@ -214,9 +216,91 @@ function syncGMap(){
     }	
 }
 
-function saveCoordinate(){
+function saveCoordinate2File(){
 	updateLocation();
-	ULeftCoodinate = new OpenSpace.MapPoint(mapPointX-1000, mapPointY+2000);
-	var blobXY = new Blob([ULeftCoodinate], {type : "text/plain;charset=utf-8"});
+	var X = mapPointX-1000;
+	var Y = mapPointY+2000;
+	//ULeftCoodinate = new OpenSpace.MapPoint(mapPointX-1000, mapPointY+2000);
+	var blobXY = new Blob([X + " " + Y], {type : "text/plain;charset=utf-8"});
 	saveAs(blobXY, "originCoordinate.txt");
+}
+
+function loadCoordinateFromFile(){
+    // Check for the various File API support.
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+      function readSingleFile(evt) {
+        //Retrieve the first (and only!) File from the FileList object
+        var f = evt.target.files[0]; 
+
+        if (f) {
+          var r = new FileReader();
+          r.onload = function(e) { 
+            var str = e.target.result;
+            alert( "Got the file\n" 
+                  +"name: " + f.name + "\n"
+                  +"type: " + f.type + "\n"
+                  +"size: " + f.size + " bytes\n"
+                  + "starts with: " + str.substr()
+            ); 
+            var res = str.split(" ");
+            easting4Trans = res[0];
+            northing4Trans = res[1]; 
+            //document.write(easting4Trans + "/" + northing4Trans);
+          }
+          r.readAsText(f);
+        } 
+        else { 
+          alert("Failed to load file");
+        }
+      }
+
+      document.getElementById('fileinput').addEventListener('change', readSingleFile, false);
+    } 
+    else {
+      alert('The File APIs are not fully supported by your browser.');
+    }
+}
+
+function getEasting4Trans() {
+	return easting4Trans;
+}
+
+function getNorthing4Trans() {
+	return northing4Trans;
+}
+
+function loadImage()
+{
+var img = new Image();
+//img.crossOrigin = 'anonymous';
+img.onload = function ()
+{
+  createCanvas(img);
+}
+/*
+1Grid: 
+upLeft   = 399000,400000
+upRight  = 400000,400000
+downLeft = 399000,399000
+downLeft = 400000,399000
+*/
+//img.src = 'screenshot.png';
+//img.src = '1Grid.png'; //upLeft   = 399000,400000
+//img.src = '4Grids.png';
+//img.src = '4Grids 406000 408005.png';
+//img.src = '9Grids 399000 401000.png';
+//img.src = 'OSMAP2.png';
+//img.src = '9Grids 405000 409995.png';
+//img.src = 'Screen Shot 2016-03-04 at 12.03.03 am.png'; //eastings=401000,northings=370000
+//img.src = 'Screen Shot 2016-03-01 at 3.21.33 pm.png'; //405005 410005
+//img.src = 'Screen Shot 2016-03-01 at 5.14.05 pm.png'; //419000 402000
+//img.src = 'Screen Shot 2016-03-01 at 3.21.33 pm.png'; //405005 410005
+//img.src = 'Screen Shot 2016-03-03 at 3.49.46 pm.png'; //eastings=401000,northings=389000
+//img.src = 'Screen Shot 2016-03-03 at 3.58.48 pm.png'; //eastings=401000,northings=389000
+//img.src = 'Screen Shot 2016-03-03 at 4.07.02 pm.png'; // eastings=407995,northings=395000
+//img.src = 'Screen Shot 2016-03-03 at 5.29.30 pm.png'; // eastings=400010,northings=400005  
+//img.src = "Screen Shot 2016-02-24 at 4.12.51 pm.png";    
+//img.src = "Screen Shot 2016-02-25 at 10.53.04 pm.png";   
+img.src = "Screen Shot 2016-03-04 at 4.11.12 pm.png"; //eastings=388005,northings=380000
+//img.src = "Screen Shot 2016-02-26 at 8.41.20 pm.png";
 }
